44625f65cd903c42dfefe1d205dd3305b0b6daf1
HellWolhardZeli <aditya11maharana@gmail.com>
2019-10-19 10:50:07 +0530

    package start app.js

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const http = require('http');
-const express = require('express');
 
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+const server = http.createServer((request, response) => {
+    response.writeHead(200, {
+        "Content-Type": "text/plain"
+    });
+    response.end("Hello World!");
 });
 
-var port = process.env.port || 3000;
-
+const port = process.env.PORT || 1337;
 server.listen(port);
+
 console.log("Server running at http://localhost:%d", port);
